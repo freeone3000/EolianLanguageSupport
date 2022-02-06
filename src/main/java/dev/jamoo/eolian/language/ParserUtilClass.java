36367b8d938dc328c759880ed3f178bfcfa21222
Jasmine Moore <freeone3000@gmail.com>
2022-02-06 13:27:21 -0500

    Stubbed out all static methods. Removed IDE-specific cruft, fixed build in IDE.

--- src/main/java/dev/jamoo/eolian/language/ParserUtilClass.java
+++ src/main/java/dev/jamoo/eolian/language/ParserUtilClass.java
@@ -1,4 +1,31 @@
 package dev.jamoo.eolian.language;
 
+import com.intellij.lang.PsiBuilder;
+import org.apache.commons.lang.NotImplementedException;
+
 public class ParserUtilClass {
+    public static boolean isNumberLiteral(PsiBuilder builder, int token) {
+        // TODO IMPLEMENT
+        throw new NotImplementedException("Not yet implemented!");
+    }
+
+    public static boolean isStringLiteral(PsiBuilder builder, int token) {
+        // TODO IMPLEMENT
+        throw new NotImplementedException("Not yet implemented!");
+    }
+
+    public static boolean isCharacterLiteral(PsiBuilder builder, int token) {
+        // TODO IMPLEMENT
+        throw new NotImplementedException("Not yet implemented!");
+    }
+
+    public static boolean isBinaryOperator(PsiBuilder builder, int token) {
+        // TODO IMPLEMENT
+        throw new NotImplementedException("Not yet implemented!");
+    }
+
+    public static boolean isUnaryOperator(PsiBuilder builder, int token) {
+        // TODO IMPLEMENT
+        throw new NotImplementedException("Not yet implemented!");
+    }
 }
